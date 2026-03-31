bdb4b4244073f1e9e46804fae43e8c0ddcf744a8
robertosl77 <63361949+robertosl77@users.noreply.github.com>
2026-03-30 23:38:15 -0300

    Update webhook processing and Flask run configuration

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 from flask import Flask, request, jsonify
 from src.menu_principal import MenuPrincipal
+
 
 app = Flask(__name__)
 sesiones = {}
@@ -9,20 +12,21 @@ def webhook():
     data = request.json
     numero = data.get('from') # Ej: "541168387770@c.us"
     texto = data.get('body')
+    texto = texto.strip().lower() if texto else ""
 
     if numero not in sesiones:
         # IMPORTANTE: MenuPrincipal NO debe tener inputs en su __init__
         sesiones[numero] = MenuPrincipal(numero)
     
     # Procesamos el comando y enviamos respuesta
-    sesiones[numero].procesar_comando(texto)
+    sesiones[numero].administro_menu(texto,sesiones)
     
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     # app.run(port=5000)
     print("🚀 Flask corriendo en http://localhost:5000")
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True, use_reloader=False)
     print("Flask ha terminado.")
 
 
@@ -37,3 +41,14 @@ if __name__ == "__main__":
 #     mp.iniciar()
 
 
+# PARA PRUEBAS SIN CELULAR
+# bash:
+# curl -X POST http://localhost:5000/webhook \
+#   -H "Content-Type: application/json" \
+#   -d "{\"from\": \"5491168387770@c.us\", \"body\": \"1\"}"
+
+# powershell:
+# Invoke-WebRequest -Uri "http://localhost:5000/webhook" `
+#   -Method POST `
+#   -ContentType "application/json" `
+#   -Body '{"from": "5491168387770@c.us", "body": "1"}'
